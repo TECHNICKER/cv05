da312195f693c8f19a1d2078d3762ec57eb438b9
Student <Student@PC-SE5113-14>
2023-10-19 08:28:43 +0200

    done

--- cv05/cv05.cpp
+++ cv05/cv05.cpp
@@ -15,9 +15,20 @@ void rozdil(double a[], double b[], double c[])
 	c[1] = a[1] - b[1];
 }
 
-void tisk(double c[2])
+void soucin(double a[], double b[], double c[])
+{
+	c[0] = ((a[0] * b[0]) - (a[1] * b[1]));
+	c[1] = ((a[0] * b[1]) + (a[1] * b[0]));
+}
+
+void tisk(double c[])
 {
 	printf("%lf + %lf i\n", c[0], c[1]);
+}
+
+void tisk2(double c[])
+{
+	printf("%lf (cos %lf + sin %lf i)\n", sqrt(powl(c[0], 2) + powl(c[1], 2)), atan(c[1]/c[0]), atan(c[1] / c[0]));
 }
 
 int main()
@@ -36,7 +47,7 @@ int main()
 		scanf("%lf + %lf i", &b[0], &b[1]);
 
 
-		printf("zvolte operaci (+ nebo -): ");
+		printf("zvolte operaci (+, -, *): ");
 		scanf(" %c", &operace);
 
 		switch (operace)
@@ -49,9 +60,14 @@ int main()
 			rozdil(a, b, c);
 			break;
 
+		case '*':
+			soucin(a, b, c);
+			break;
+
 		}
 
 		tisk(c);
+		tisk2(c);
 
 	}
 	return 0;
